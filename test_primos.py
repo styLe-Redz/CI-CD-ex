5a18e16e617844a4d8888997ec649253df0442af
Christian Jiménez <101473794+styLe-Redz@users.noreply.github.com>
2023-08-22 18:01:22 +0200

    Update test_primos.py

--- test_primos.py
+++ test_primos.py
@@ -1,18 +1,11 @@
 from es_primo import function_es_primo
 
 def test_es_primo():
-    assert function_es_primo(2) == True
-    assert function_es_primo(3) == True
-    assert function_es_primo(5) == True
-    assert function_es_primo(4) == False
-    assert function_es_primo(9) == False
-    assert function_es_primo(11) == True
-    assert function_es_primo(15) == False
+    assert function_es_primo(2) 
+    assert function_es_primo(3)
+    assert function_es_primo(5) 
+    assert not function_es_primo(4) 
+    assert not function_es_primo(9) 
+    assert function_es_primo(11)
+    assert not function_es_primo(15) 
 
-def test_numeros_negativos():
-    assert function_es_primo(-7) == False
-    assert function_es_primo(-2) == False
-
-def test_numeros_pequenos():
-    assert function_es_primo(0) == False
-    assert function_es_primo(1) == False
