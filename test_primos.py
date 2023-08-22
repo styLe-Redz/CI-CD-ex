75c2967a7128d3af469ca2238c5d0b80072fabd1
styLe-Redz <christian98.117@gmail.com>
2023-08-22 15:54:41 -0500

    pytest funcion primo

--- test_primos.py
+++ test_primos.py
@@ -1,21 +1,21 @@
 from es_primo import function_es_primo
 
 def test_es_primo():
-    assert es_primo(2) == True
-    assert es_primo(3) == True
-    assert es_primo(5) == True
-    assert es_primo(4) == False
-    assert es_primo(9) == False
-    assert es_primo(11) == True
-    assert es_primo(15) == False
+    assert function_es_primo(2) == True
+    assert function_es_primo(3) == True
+    assert function_es_primo(5) == True
+    assert function_es_primo(4) == False
+    assert function_es_primo(9) == False
+    assert function_es_primo(11) == True
+    assert function_es_primo(15) == False
 
 def test_numeros_negativos():
-    assert es_primo(-7) == False
-    assert es_primo(-2) == False
+    assert function_es_primo(-7) == False
+    assert function_es_primo(-2) == False
 
 def test_numeros_pequenos():
-    assert es_primo(0) == False
-    assert es_primo(1) == False
+    assert function_es_primo(0) == False
+    assert function_es_primo(1) == False
 
 if __name__ == "__main__":
     pytest.main()
